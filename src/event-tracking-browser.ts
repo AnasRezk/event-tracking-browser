2fafdf4f16e8521c8eef51f07314284b4590aa65
anasrezk <anas.rezk.js@gmail.com>
2020-03-21 23:10:50 +0200

    setup skeleton functions names

--- src/event-tracking-browser.ts
+++ src/event-tracking-browser.ts
@@ -1,4 +1,43 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-// import "core-js/fn/array.find"
-// ......
-export default class DummyClass {}
+import axios from 'axios';
+
+interface EventTrackerOptions {}
+
+/**
+ * This function initializes a new instance of the tracking object
+ * @param token API token.
+ * @param config A dictionary of config options to override default config.
+ * @returns distinctId.
+ */
+const init = async (token: string, config?: EventTrackerOptions) => {
+    try {
+        const response = await axios.get(`/api/init?token=${token}`);
+        let { data } = response.data;
+        return data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+/**
+ * Track an event
+ * @param eventName The name of the event. This can be anything the user does - 'Button Click', 'Sign Up', etc.
+ * @param properties A set of properties to include with the event you're sending
+ * @returns If the tracking request was successfully initiated/queued,
+ * an object with the tracking payload sent to the API server is returned; otherwise false.
+ */
+const track = (eventName: string, properties: any) => {};
+
+/**
+ * Register a set of super properties, which are included with all events.
+ * @param properties  An associative array of properties to store about the user
+ */
+const register = (properties: any) => {};
+
+/**
+ * Identify a user with a unique ID to track user activity across devices, tie a user to their events, and create a user profile. If you never call this method, unique visitors are tracked using a UUID generated the first time they visit the site.
+ * @param uniqueId A string that uniquely identifies a user.
+ * If not provided, the distinctId currently in the persistent store (cookie or localStorage) will be used.
+ */
+const identify = (uniqueId?: string) => {};
+
+export { init, track, register, identify };
